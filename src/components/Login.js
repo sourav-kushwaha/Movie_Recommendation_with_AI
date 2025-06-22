ded0505a5f8b260072b7ca7b4330984440ca8a50
Sourav Singh Kushwaha <souravsinghkushwaha20@gmail.com>
2025-06-22 19:44:15 +0530

    firebase authentication done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../utils/firebase";
 
 const Login = () => {
   const[isSignInForm , setIsSignInForm]=useState(true);
@@ -8,11 +10,44 @@ const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
-  const number = useRef(null);
+
 
   const handleButtonClick = ()=>{
-    const message = checkValidData(email.current.value , password.current.value, name.current.value, number.current.value)
+    const message = checkValidData(email.current.value , password.current.value)
     setErrorMessage(message);
+    if(message){
+    return;
+    }
+    if(!isSignInForm){
+createUserWithEmailAndPassword(auth, email.current.value , password.current.value)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode+" "+errorMessage);
+    // ..
+  });
+    }
+    else {
+      signInWithEmailAndPassword(auth, email.current.value , password.current.value)
+       .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode+" "+errorMessage);
+  });
+
+    }
   }
    const toggleSignInForm = ()=>{
     setIsSignInForm(!isSignInForm);
@@ -25,9 +60,7 @@ const Login = () => {
     </div> 
     <form className= 'absolute bg-black w-3/12  p-12 my-44 mx-auto left-0 right-0 bg-opacity-80  rounded-lg'>
     <h1 className='font-bold text-white text-3xl py-4'>{isSignInForm?"Sign In":"Register"} </h1>
-     {!isSignInForm&&(<input ref={name} type='text' placeholder='Full Name'  className='p-2 my-4 w-full bg-gray-700'/> )}
-     
- {!isSignInForm && (<input ref={number} type='text' placeholder='Mobile Number' className='p-2 my-4 w-full bg-gray-700'/>)}
+     {!isSignInForm && (<input ref={name} type='text' placeholder='Full Name'  className='p-2 my-4 w-full bg-gray-700'/> )}
       <input ref={email} type='text' placeholder='Email Address' className='p-2 my-4 w-full bg-gray-700 '/>
       <input ref={password} type='password' placeholder='Password'  className='p-2 my-4 w-full bg-gray-700'/> 
       <p className='text-red-600'>{errorMessage}</p>
